fa9a0bbc209500a06f2b03784672735cbeb91ca3
Zentality <jmcp@live.com.au>
2022-05-20 13:32:23 +0800

    Add isActive variable to projects

--- src/displayController.js
+++ src/displayController.js
@@ -98,6 +98,7 @@ let displayController = (() => {
     firstProject.classList.add("active");
   }
   initialPageLoad();
+  
 
 })();
 
--- src/projects.js
+++ src/projects.js
@@ -1,39 +1,55 @@
-let todoItem = (title, description, dueDate, priority) => {
+const todoItem = (title, description, dueDate, priority) => {
 
   return {title, description, dueDate, priority};
 }
 
+
+
 let project = (title, desc, dueDate, priority) => {
-  let todoList = [];
-  let getToDoList = () => {
+  let isActive = false;
+  const todoList = [];
+  const getToDoList = () => {
     return todoList.slice();
   }
-  let addToList = (title, description, dueDate, priority) => {
+  const addToList = (title, description, dueDate, priority) => {
     todoList.push(todoItem(title, description, dueDate, priority));
   }
   
-  return {title, desc, dueDate, priority, getToDoList, addToList};
+  return {title, desc, dueDate, priority, isActive, getToDoList, addToList};
 }
 
-let projects = (() => {
-  let listOfProjects = [];
-  let getListOfProjects = () => {
+const projects = (() => {
+  const listOfProjects = [];
+  const getListOfProjects = () => {
     return listOfProjects.slice();
   }
 
-  let addProject = (title, desc, dueDate, priority) => {
+  const addProject = (title, desc, dueDate, priority) => {
     listOfProjects.push(project(title, desc, dueDate, priority));
+    if (listOfProjects.length === 1){
+      setActiveProject(0);
+    }
   }
 
-  let removeProject = (index) => {
+  const removeProject = (index) => {
     listOfProjects.splice(index, 1);
+  }
+
+  const setActiveProject = (index) => {
+    listOfProjects.forEach((project) => {
+      project.isActive = false;
+    })
+    listOfProjects[index].isActive = true;
   }
   
   return {getListOfProjects, addProject, removeProject};
 })();
 
+
+
 projects.addProject("Example Project", "This is a description for a project example, to get started making your own projects click the plus button on the projects panel", "2022-05-05", "1");
 projects.addProject("Test Project2", "Description for test project 2", "2022-05-05", "2");
 projects.addProject("Test Project3", "Description for test project 3", "2022-05-05", "1");
+console.log(projects.getListOfProjects());
 
 export default projects;
