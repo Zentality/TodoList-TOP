a1e0562212940be5a9f345d5eb12941ff47266d0
Zentality <jmcp@live.com.au>
2022-05-24 16:59:53 +0800

    Implement sort by priority

--- src/projects.js
+++ src/projects.js
@@ -27,7 +27,21 @@ const project = (title, desc, dueDate, priority) => {
     } else if (method === 1) {
       todoList = todoList.sort((a, b) => a.dueDate.localeCompare(b.dueDate));
     } else if (method === 2) {
-      console.log({ a, b });
+      todoList = todoList.sort((b, a) => {
+        let aValue = 0;
+        let bValue = 0;
+        if (a.priority === "High") {
+          aValue = 2;
+        } else if (a.priority === "Medium") {
+          aValue = 1;
+        }
+        if (b.priority === "High") {
+          bValue = 2;
+        } else if (b.priority === "Medium") {
+          bValue = 1;
+        }
+        return aValue - bValue;
+      });
     }
   };
 
@@ -75,8 +89,13 @@ const projects = (() => {
 
 // These are just preloaded example projects
 projects.addProject("Sample Project", "This is a description for a project example, to get started making your own projects click the plus button on the projects panel", "2022-05-05", "High");
+projects.getListOfProjects()[0].addToList("Example todo", "Hey I hope you enjoy this project, feel free to delete this todo it is only an example", "2022-04-04", "Medium");
+projects.getListOfProjects()[0].addToList("Bnother one", "Also an example, anyway, if you would like to encourage me a follow on github couldn't hurt! The link is in the footer", "2022-05-05", "High");
 projects.getListOfProjects()[0].addToList("Example todo", "Hey I hope you enjoy this project, feel free to delete this todo it is only an example", "2022-04-04", "Low");
+projects.getListOfProjects()[0].addToList("Another one", "Also an example, anyway, if you would like to encourage me a follow on github couldn't hurt! The link is in the footer", "2022-05-05", "Medium");
+projects.getListOfProjects()[0].addToList("Cxample todo", "Hey I hope you enjoy this project, feel free to delete this todo it is only an example", "2022-04-04", "Low");
 projects.getListOfProjects()[0].addToList("Another one", "Also an example, anyway, if you would like to encourage me a follow on github couldn't hurt! The link is in the footer", "2022-05-05", "High");
+
 projects.addProject("Sample 2", "This has no todos, but shows how the project bar looks", "2022-05-05", "Medium");
 
 export default projects;
